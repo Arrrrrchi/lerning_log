815e2bf4f98549c97e7e71f8d2570865360ca60e
Arrrrrchi <esh6c4nm@gmail.com>
2024-05-18 11:24:49 +0900

    トピックの一覧ページとトピックの詳細ページの作成

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+def index(request):
+    """学習ノートのホームページ"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """すべてのトピックを表示する"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """1つのトピックとそれについての全ての記事を表示"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
